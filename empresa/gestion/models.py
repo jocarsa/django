e3dea760e35fb5ab840dde647bdac723d18b7037
jocarsa <info@josevicentecarratala.com>
2025-06-09 13:01:20 +0200

    5

--- empresa/gestion/models.py
+++ empresa/gestion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20)
+    direccion = models.TextField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellidos}"
